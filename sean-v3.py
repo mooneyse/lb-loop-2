6cc283a1b8640d601b71de19769d2973ae94fae8
mooneyse <mooneyse@tcd.ie>
2018-07-30 12:45:42 +0100

    edit

--- sean-v3.py
+++ sean-v3.py
@@ -1,41 +1,74 @@
 #!/usr/bin/env python3
 
-import argparse
+import argparse, h5py
 import numpy as np
+from losoto.h5parm import h5parm as losoto_h5parm
+from losoto.h5parm import openSoltab
 
 def evaluate_solutions(h5parm, master): # subroutine 1
     ''' input:    h5parm with phase solutions
-        function: evaluate solutions for each antenna; use XX-YY statistic
-                  described in the Hybrid mapping section of the Google Doc and
+        function: evaluate solutions for each antenna; use xx-yy statistic
+                  described in the hybrid mapping section of the google doc and
                   determine validity
-        output:   Append to master file with h5parm_name, RA, dec, and one
+        output:   Append to master file with h5parm_name, ra, dec, and one
                   column per antenna with boolean for validity
     '''
 
-    ra = 123
-    dec = 456
-    evaluations = ['y', 'n', 'y']
+    # get (ra, dec) for the source from the h5parm
+    H = h5py.File(h5parm, 'r')
+    ra_dec = H['/sol000/source'][0][1] # [ra, dec] in degrees
+
+    # get the phase solutions for each antenna from the h5parm
+    h = losoto_h5parm(h5parm, readonly = True)
+    phase = openSoltab(h5parm, 'sol000', 'phase000')
+
+    stations = phase.ant[:]
+
+    # loop over all stations and for each polarisation, I have a list with one
+    # element per per time step, and each time step is a list of values with one
+    # for each subband
+    for station in range(len(stations)):
+        xx, yy = [], []
+
+        # [polarisation (xx = 0, yy  = 1), direction (0), station, frequency, time]
+        for value in phase.val[0, 0, station, :, :]:
+            xx.append(value)
+
+        for value in phase.val[1, 0, station, :, :]:
+            yy.append(value)
+
+    a = xx[0]
+    print(len(xx))
+    print(a)
+    print(a[0])
+    print(len(a))
+
+    # scatter in XX-YY phase (get all phase solutions on a station, subtract
+    # XX  and yy, reduce difference from -180 to 180, take the mean of the
+    # absolute difference value and multiply by 2/pi)
+
+    evaluations = [1, 0, 1]
 
     # append to master file
     with open(master, 'a') as f:
-        f.write('{}, {}, {}'.format(h5parm, ra, dec))
+        f.write('{}, {}, {}'.format(h5parm, ra_dec[0], ra_dec[1]))
         for e in evaluations:
             f.write(', {}'.format(e))
         f.write('\n')
 
-def make_h5parm():
+def make_h5parm(): # subroutine 2
     ''' input:    direction of measurement set to be self-calibrated; master
                   file with list of h5parms
         function: find nearest directions; construct a new h5parm that is a
                   combination of best phase solutions from nearest direction,
                   done per antenna. e.g. if the nearest direction has valid
-                  solutions for all but the UK antenna, find the UK solutions
+                  solutions for all but the uk antenna, find the uk solutions
                   from the next nearest direction
         output:   a new h5parm to be applied to the measurement set
     '''
     pass
 
-def apply_h5parm():
+def applyh5parm(): # subroutine 3
     ''' input:    the output of make_h5parm; the measurement set for self-
                   calibration
         function: apply the output of make_h5parm to the measurement set
@@ -43,7 +76,7 @@ def apply_h5parm():
     '''
     pass
 
-def comibine_h5parm():
+def updatelist(): # subroutie 4
     ''' input:    the initial h5parm (i.e. from make_h5parm); the final h5parm
                   from loop 3
         function: combine the phase solutions from the initial h5parm and the
@@ -55,12 +88,11 @@ def comibine_h5parm():
 
     # update the master file
     # evaluate_solutions()
-
     pass
 
 def main():
-    ''' Starting point: 1st iteration will have produced phase solutions
-        independently for all directions, with eachbeing its own h5parm
+    ''' starting point: 1st iteration will have produced phase solutions
+        independently for all directions, with each being its own h5parm
     '''
 
     # parse command-line arguments
@@ -76,11 +108,11 @@ def main():
 
     make_h5parm()
 
-    apply_h5parm()
+    applyh5parm()
 
     # loop 3
 
-    comibine_h5parm()
+    updatelist()
 
 if __name__ == '__main__':
     main()
