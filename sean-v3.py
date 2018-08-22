3add9a6a13d5c362688cf46e2a9b9923b0edfe6a
mooneyse <mooneyse@tcd.ie>
2018-08-22 17:58:49 +0100

    asdf

--- sean-v3.py
+++ sean-v3.py
@@ -217,6 +217,7 @@ def make_h5parm(mtf, ms, clobber = False):
     phase = lo.getSolset('sol000').getSoltab('phase000')
     for s in phase.ant[:]: # stations
         if s == my_station:
+            print(s)
             logging.info('{} has {} dimensions, (pol, dir, ant, freq, time): {}'.format(my_h5parm, phase.val.ndim, phase.val.shape))
             # polVals = phase.val[:,0,0,0,0] #  polarisation
             # dirVals = phase.val[0,:,0,0,0] # direction
