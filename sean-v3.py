076e0fb3455a2c3541adb6da3200abcc1f3d4a78
mooneyse <mooneyse@tcd.ie>
2018-08-27 15:45:56 +0200

    asdf

--- sean-v3.py
+++ sean-v3.py
@@ -215,7 +215,7 @@ def make_h5parm(mtf, ms, clobber = False):
 
     working_file_data = np.genfromtxt(working_file, delimiter='\t', dtype = str)
     for bloop in range(len(working_file_data)):
-        print(working_file_data[bloop][0], working_file_data[bloop][len(working_file_data[bloop])])
+        print(working_file_data[bloop][0], working_file_data[bloop][4])
 
     # get the station for which the result is valid
     # TODO right now, just taking one station by way of example
