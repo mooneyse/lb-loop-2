32719c5607a0973307830bbec0600de4320ca57d
mooneyse <mooneyse@tcd.ie>
2018-08-29 14:57:09 +0200

    asdf

--- sean-v3.py
+++ sean-v3.py
@@ -196,11 +196,8 @@ def make_h5parm(mtf, ms, clobber = False, directions = []):
         t.close()
 
     else: # NB just taking first one for now!
-        # directions = np.array(directions)
         for ra, dec in zip(directions[0], directions[1]):
-        # for ra, dec in zip(directions[:, 0], directions[:, 1]):
-            print('+++++++++++++++++++++',ra, dec)
-            directions = SkyCoord(ra=ra, dec=dec, unit = 'deg')
+            directions = SkyCoord(ra, dec, unit = 'deg')
             logging.info('source positions given, using ra {}, dec {}'.format(directions.ra.deg, directions.dec.deg))
 
     # get the direction from the master text file
