a1a3d8e7943738b85d98a66fef83d6133a0a7612
mooneyse <mooneyse@tcd.ie>
2019-04-16 12:12:13 +0100

    asdf

--- hdf5_functions.py
+++ hdf5_functions.py
@@ -246,8 +246,8 @@ def dir2phasesol(mtf, ms='', directions=[]):
     # write these best phase solutions to the new h5parm
     print('list(vals.shape):', list(vals.shape))
     c = solset.makeSoltab('phase',
-                          axesNames=['pol', 'ant', 'freq', 'time'],  # 'dir',
-                          axesVals=[pol, ant, freq, time],  # dir,
+                          axesNames=['time', 'freq', 'ant', 'pol'],  # 'dir',
+                          axesVals=[time, freq, ant, pol],  # dir,
                           vals=vals,
                           weights=weights)  # creates phase000
 
