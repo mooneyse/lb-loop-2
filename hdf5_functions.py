acdb7c568239c845d7bdf815d267d23c8baee116
mooneyse <mooneyse@tcd.ie>
2019-04-16 13:38:34 +0100

    asdf

--- hdf5_functions.py
+++ hdf5_functions.py
@@ -69,8 +69,9 @@ def evaluate_solutions(h5parm, mtf, solution_table, threshold=0.25):
     solname = h.getSolsetNames()[0]  # set to -1 to use only the last solset
     solset = h.getSolset(solname)
     soltabnames = solset.getSoltabNames()
-    print('SOLTABNAMES:', soltabnames)
     soltab = solset.getSoltab(solution_table + '000')
+    asdf = soltab.getSolsetNames(soltab)
+    print('SOLTABNAME:', asdf)
     stations = soltab.ant
     source = solset.getSou()  # dictionary
     direction = np.degrees(np.array(source[list(source.keys())[0]]))  # degrees
