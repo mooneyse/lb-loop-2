c92104bd8bef21c79af3b7f5ff65a2858e4c7999
mooneyse <mooneyse@tcd.ie>
2019-05-03 18:02:07 +0100

    asdf

--- hdf5_functions.py
+++ hdf5_functions.py
@@ -289,6 +289,7 @@ def dir2phasesol(mtf, ms='', directions=[]):
         time_mins.append(np.min(time))
         time_maxs.append(np.max(time))
         time_steps.append((np.max(time) - np.min(time)) / (len(time) - 1))
+        print('HEY...', np.min(time), np.max(time), (np.max(time) - np.min(time)) / (len(time) - 1))
 
         frequencies.append(phase.freq[:])
         ant = phase.ant[:]
@@ -315,8 +316,8 @@ def dir2phasesol(mtf, ms='', directions=[]):
     # properties of the new h5parm
     num_of_steps = (np.max(time_maxs) - np.min(time_mins)) / np.min(time_steps)
     time = np.linspace(np.min(time_mins), np.max(time_maxs), num_of_steps)
-    print('HEY...', np.min(time_mins), np.max(time_maxs), num_of_steps)
-    print('TIMEBIGSHAPE', time.shape)
+    print('BIGHEY...', np.min(time_mins), np.max(time_maxs), num_of_steps)
+    print('BIGTIMESHAPE', time.shape)
 
     freq = [np.average(frequencies)]  # all items in the list should be equal
     pol = ['XX', 'YY']  # as standard
