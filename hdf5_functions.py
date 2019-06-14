0a36da919f72bd48df96222ea1c7affd3e34ccce
mooneyse <mooneyse@tcd.ie>
2019-06-14 16:39:01 +0100

    asdf

--- hdf5_functions.py
+++ hdf5_functions.py
@@ -506,7 +506,7 @@ def dir2phasesol(mtf, ms='', directions=[]):
     dir_ = [str(directions.ra.rad) + ', ' + str(directions.dec.rad)]  # given
 
     vals = np.concatenate(val, axis=2)
-    print('val shape', vals.shape, vals[0,0,23,0,0], ant[23]])
+    print('val shape', vals.shape, vals[0,0,23,0,0], ant[23])
     weights = np.concatenate(weight, axis=2)
     # TODO the HACK on the line below is necessary to get around the fact that
     #      there are three frequencies
