763ef51d2d83e221b266722299d476ea96ab0ba9
mooneyse <mooneyse@tcd.ie>
2019-06-26 16:21:18 +0100

    uʍop ǝpᴉsdn sɐʍ ʇdᴉɹɔs

--- hdf5_functions.py
+++ hdf5_functions.py
@@ -940,8 +940,8 @@ def add_amplitude_and_phase_solutions(diag_A_1, diag_P_1, diag_A_2, diag_P_2):
     diag_P_2 = np.nan_to_num(diag_P_2)
 
     if diag_A_1.ndim > 1:  # more than one frequency axis
-        amplitude_final = np.zeroes(diag_A_1.shape)
-        phase_final = np.zeroes(diag_P_1.shape)
+        amplitude_final = np.zeros(diag_A_1.shape)
+        phase_final = np.zeros(diag_P_1.shape)
 
         for i in diag_A_1.shape[1]:
             amplitude_1_2, phase_1_2 = [], []
